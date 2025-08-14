d86ae60a34a01c3fe0c76ed60833f5ac353532fa
Yudi Yamada <yudiyamada10@gmail.com>
2025-08-14 15:02:08 -0300

    ajustando o mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
+/*Calcular altura */
+function ajustaAltura() {
+  const alturaReal = window.innerHeight;
+  document.documentElement.style.setProperty(
+    "--altura-real",
+    `${alturaReal}px`
+  );
+}
+
+window.addEventListener("resize", ajustaAltura);
+window.addEventListener("load", ajustaAltura);
+
 /*Seção Home*/
 const navLinks = document.querySelectorAll("header nav a");
 const logoLink = document.querySelector(".logo");
@@ -6,121 +18,125 @@ const menuIcon = document.querySelector("#menu-icon");
 const navbar = document.querySelector("header nav");
 
 menuIcon.addEventListener("click", () => {
-    menuIcon.classList.toggle("bx-x");
-    navbar.classList.toggle("active");
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 });
 
 const activePage = () => {
-    const header = document.querySelector("header");
-    const barsBox = document.querySelector(".bars-box");
+  const header = document.querySelector("header");
+  const barsBox = document.querySelector(".bars-box");
 
+  header.classList.remove("active");
+  setTimeout(() => {
+    header.classList.add("active");
+  }, 1100);
 
-    header.classList.remove("active");
-    setTimeout(() => {
-        header.classList.add("active");
-    }, 1100);
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
 
-    navLinks.forEach(link => {
-        link.classList.remove("active");
-    });
+  barsBox.classList.remove("active");
+  setTimeout(() => {
+    barsBox.classList.add("active");
+  }, 1100);
 
-    barsBox.classList.remove("active");
-    setTimeout(() => {
-        barsBox.classList.add("active");
-    }, 1100);
+  sections.forEach((section) => {
+    section.classList.remove("active");
+  });
 
-    sections.forEach(section => {
-        section.classList.remove("active");
-    });
-
-    menuIcon.classList.remove("bx-x");
-    navbar.classList.remove("active");
-}
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
 navLinks.forEach((link, idx) => {
-    link.addEventListener("click", () => {
-        if(!link.classList.contains("active")){
-            activePage();
+  link.addEventListener("click", () => {
+    if (!link.classList.contains("active")) {
+      activePage();
 
-            link.classList.add("active");
+      link.classList.add("active");
 
-            setTimeout(() => {
-                sections[idx].classList.add("active");
-            }, 1100);
-        }
-    });
+      setTimeout(() => {
+        sections[idx].classList.add("active");
+      }, 1100);
+    }
+  });
 });
 
 logoLink.addEventListener("click", () => {
-    if(!navLinks[0].classList.contains("active")){
-        activePage();
+  if (!navLinks[0].classList.contains("active")) {
+    activePage();
 
-        navLinks[0].classList.add("active");
+    navLinks[0].classList.add("active");
 
-        setTimeout(() => {
-            sections[0].classList.add("active");
-        }, 1100);
-    }
-})
-
+    setTimeout(() => {
+      sections[0].classList.add("active");
+    }, 1100);
+  }
+});
 
 /*Seção Resumo*/
 const resumeBtns = document.querySelectorAll(".resume-btn");
 
 resumeBtns.forEach((btn, idx) => {
-    btn.addEventListener("click", () => {
-        const resumeDetails = document.querySelectorAll(".resume-detail")
+  btn.addEventListener("click", () => {
+    const resumeDetails = document.querySelectorAll(".resume-detail");
 
-        resumeBtns.forEach(btn => {
-            btn.classList.remove("active");
-        });
-        btn.classList.add("active");
-
-        resumeDetails.forEach(detail => {
-            detail.classList.remove("active");
-        });
-        resumeDetails[idx].classList.add("active");
+    resumeBtns.forEach((btn) => {
+      btn.classList.remove("active");
     });
+    btn.classList.add("active");
+
+    resumeDetails.forEach((detail) => {
+      detail.classList.remove("active");
+    });
+    resumeDetails[idx].classList.add("active");
+  });
 });
 
 /*Seção Projetos*/
-const arrowRight = document.querySelector(".projects-box .navigation .arrow-right");
-const arrowLeft = document.querySelector(".projects-box .navigation .arrow-left");
+const arrowRight = document.querySelector(
+  ".projects-box .navigation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".projects-box .navigation .arrow-left"
+);
 
 let index = 0;
 
 const activeProjects = () => {
-    const imgSlider = document.querySelector(".projects-carrossel .img-slide");
-    const projectsDetails = document.querySelectorAll(".projects-detail");
+  const imgSlider = document.querySelector(".projects-carrossel .img-slide");
+  const projectsDetails = document.querySelectorAll(".projects-detail");
 
-    imgSlider.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem))`;
+  imgSlider.style.transform = `translateX(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
 
-    projectsDetails.forEach(detail => {
-        detail.classList.remove("active");
-    });
-    projectsDetails[index].classList.add("active");
-}
+  projectsDetails.forEach((detail) => {
+    detail.classList.remove("active");
+  });
+  projectsDetails[index].classList.add("active");
+};
 
 arrowRight.addEventListener("click", () => {
-    if (index < 4){
-        index++;
-        arrowLeft.classList.remove("disabled");
-    }else{
-        index = 5;
-        arrowRight.classList.add("disabled");
-    }
+  if (index < 4) {
+    index++;
+    arrowLeft.classList.remove("disabled");
+  } else {
+    index = 5;
+    arrowRight.classList.add("disabled");
+  }
 
-    activeProjects();
+  activeProjects();
 });
 
 arrowLeft.addEventListener("click", () => {
-    if (index > 1){
-        index--;
-        arrowRight.classList.remove("disabled");
-    }else{
-        index = 0;
-        arrowLeft.classList.add("disabled");
-    }
+  if (index > 1) {
+    index--;
+    arrowRight.classList.remove("disabled");
+  } else {
+    index = 0;
+    arrowLeft.classList.add("disabled");
+  }
 
-    activeProjects();
+  activeProjects();
 });
